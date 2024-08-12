38b7391d2889de594b987091cd8de3d3d8b1180e
ext-roy.sefton@flowbird.group <ext-roy.sefton@flowbird.group>
2024-08-12 14:26:31 +0100

    password input added

---
+++ src/components/features/password/PasswordInput.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import styles from "./styles/password-input.css";
+
+interface PasswordInputProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  required?: boolean;
+}
+
+const PasswordInput: React.FC<PasswordInputProps> = ({
+  value,
+  onChange,
+  placeholder = "Password",
+  required = true,
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className={styles.passwordContainer}>
+      <input
+        type={showPassword ? "text" : "password"}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        required={required}
+        className={styles.input}
+      />
+      <span
+        className={styles.passwordToggleIcon}
+        onClick={togglePasswordVisibility}
+      >
+        <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+      </span>
+    </div>
+  );
+};
+
+export default PasswordInput;
