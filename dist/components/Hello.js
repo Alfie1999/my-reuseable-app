ee8719a81c402d55807c7b777c34c63b2fe9e0c5
ext-roy.sefton@flowbird.group <ext-roy.sefton@flowbird.group>
2024-08-13 10:57:07 +0100

    updated

--- dist/components/Hello.js
+++ dist/components/Hello.js
@@ -1,5 +1,17 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import { jsx as _jsx } from "react/jsx-runtime";
+import styles from "./password-input.module.css";
 export var Hello = function () {
-    return _jsx("h1", { children: "Hello, World!" });
+    return (_jsx("h1", { children: _jsx("div", __assign({ className: styles.passwordContainer }, { children: "Hello, World!" })) }));
 };
 export default Hello;
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,8 +1,13 @@
 // src/components/Hello.tsx
 import React from "react";
+import styles from "./password-input.module.css";
 
 export const Hello: React.FC = () => {
-  return <h1>Hello, World!</h1>;
+  return (
+    <h1>
+      <div className={styles.passwordContainer}>Hello, World!</div>
+    </h1>
+  );
 };
 
 export default Hello;
