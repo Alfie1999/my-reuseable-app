734937b257e709a598d28cf7fe36d84f7a23e8a8
ext-roy.sefton@flowbird.group <ext-roy.sefton@flowbird.group>
2024-08-13 13:18:15 +0100

    updated

--- src/components/PasswordInput2.tsx
+++ src/components/PasswordInput2.tsx
@@ -1,14 +1,6 @@
 // src/components/Hello.tsx
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
 import styles from "./password-input.module.css";
-
-interface PasswordInputProps2 {
-  value: string;
-  placeholder?: string;
-  required?: boolean;
-}
 
 export const PasswordInput2: React.FC = () => {
   return (
