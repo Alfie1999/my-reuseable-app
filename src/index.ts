ed76c99b9a7ecb701a8caa220d0f509f261e75a9
ext-roy.sefton@flowbird.group <ext-roy.sefton@flowbird.group>
2024-08-13 09:24:17 +0100

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 // src/index.ts
-export { default as PasswordInput } from "./components/PasswordInput";
+export { default as PasswordInput } from "./components/password/PasswordInput";
 export { default as Hello } from "./components/Hello";
